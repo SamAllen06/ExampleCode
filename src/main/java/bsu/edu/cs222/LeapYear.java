5a84c689ea352f043c806db84ca99c7f1def7855
SamAllen06 <samgallen06@gmail.com>
2025-08-27 22:08:28 -0400

    simplified if statements

--- src/main/java/bsu/edu/cs222/LeapYear.java
+++ src/main/java/bsu/edu/cs222/LeapYear.java
@@ -8,11 +8,8 @@ public class LeapYear {
         else if (year%100 == 0){
             return false;
         }
-        else if (year%4 == 0){
-            return true;
-        }
-        else{
-            return false;
+        else {
+            return year%4 == 0;
         }
     }
 }
